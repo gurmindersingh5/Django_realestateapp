746c2bfb7ef8892b1dbacd6c864b43e4e5d87796
Gurminder singh <123150161+gurmindersingh5@users.noreply.github.com>
2024-08-12 00:24:14 -0400

    Delete customerlist directory

--- customerlist/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.customerlist, name='customerlist'),
-    path('customerlist/<int:cust_id>/', views.customerlist, name='customerlist'),
-
-]
